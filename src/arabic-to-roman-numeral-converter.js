45e666ff1bf6645e3ab66ec3656f27e7e706c555
Dan Parkin <hello@danparkin.com>
2019-08-03 19:11:49 +0100

    refactor: simplify conditionals

--- src/arabic-to-roman-numeral-converter.js
+++ src/arabic-to-roman-numeral-converter.js
@@ -3,9 +3,7 @@ export default (number) => {
     const numeralOne = 'I'
     const numeralFive = 'V'
 
-    if (number === 8) return numeralFive + numeralOne + numeralOne + numeralOne
-    if (number === 7) return numeralFive + numeralOne + numeralOne
-    if (number === 6) return numeralFive + numeralOne
+    if (number > 5) return numeralFive + numeralOne.repeat(number - 5)
     if (number === 5) return numeralFive
     if (number === 4) return numeralOne + numeralFive
     return numeralOne.repeat(number)
