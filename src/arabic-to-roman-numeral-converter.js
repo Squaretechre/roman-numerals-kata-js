e56456154d400e90f58eb59f401a2310b882a320
Dan Parkin <hello@danparkin.com>
2019-08-03 19:15:45 +0100

    refactor: extract named constant for numeral ten

--- src/arabic-to-roman-numeral-converter.js
+++ src/arabic-to-roman-numeral-converter.js
@@ -3,8 +3,9 @@ export default (number) => {
     const numeralOne = 'I'
     const numeralFive = 'V'
     const numeralNine = 'IX'
+    const numeralTen = 'X'
 
-    if (number === 10) return 'X'
+    if (number === 10) return numeralTen
     if (number === 9) return numeralNine
     if (number > 5) return numeralFive + numeralOne.repeat(number - 5)
     if (number === 5) return numeralFive
