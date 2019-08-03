c2a55ab6e7f006ca94590aa7a2ac8535e2a10b6b
Dan Parkin <hello@danparkin.com>
2019-08-03 19:22:01 +0100

    refactor: introduce constant for numeral four

--- src/arabic-to-roman-numeral-converter.js
+++ src/arabic-to-roman-numeral-converter.js
@@ -1,12 +1,13 @@
 
 export default (number) => {
     const numeralOne = 'I'
+    const numeralFour = 'IV'
     const numeralFive = 'V'
     const numeralNine = 'IX'
     const numeralTen = 'X'
 
     if (number === 15) return numeralTen + numeralFive
-    if (number === 14) return numeralTen + numeralOne + numeralFive
+    if (number === 14) return numeralTen + numeralFour
     if (number === 13) return numeralTen + numeralOne + numeralOne + numeralOne
     if (number === 12) return numeralTen + numeralOne + numeralOne
     if (number === 11) return numeralTen + numeralOne
@@ -14,6 +15,6 @@ export default (number) => {
     if (number === 9) return numeralNine
     if (number > 5) return numeralFive + numeralOne.repeat(number - 5)
     if (number === 5) return numeralFive
-    if (number === 4) return numeralOne + numeralFive
+    if (number === 4) return numeralFour
     return numeralOne.repeat(number)
 }
